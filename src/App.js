aee576d9145942dc9f9d2666b227b1b54035d311
hkthirano <hirano.hkt@gmail.com>
2023-11-12 13:54:30 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+/// app.js
 import React from 'react';
-import './style.css';
+import DeckGL from '@deck.gl/react';
+import {LineLayer} from '@deck.gl/layers';
 
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
+
+// Data to be used by the LineLayer
+const data = [
+  {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
+];
+
+// DeckGL react component
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  const layers = [
+    new LineLayer({id: 'line-layer', data})
+  ];
+
+  return <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      layers={layers} />;
 }
